7af89e09f7bd60be8fe91655f1e7ffa9d52d7d0b
Simen <simenhol@uia.no>
2023-03-24 13:33:18 +0100

    endret kode

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	var input string
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		input = scanner.Text()
+		if input == "q" || input == "exit" {
+			fmt.Println("exit")
+			os.Exit(0)
+		} else if input == "convert" {
+			fmt.Println("Konverterer alle målingene gitt i grader Celsius til grader Fahrenheit.")
+			// funksjon som åpner fil, leser linjer, gjør endringer og lagrer nye linjer i en ny fil
+
+			// flere else-if setninger
+		} else {
+			fmt.Println("Venligst velg convert, average eller exit:")
+
+		}
+
+	}
+
+	//if err != nil {
+	//		log.Fatal(err)
+	//	}
+}
